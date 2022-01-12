8480dc1ed68ac661ec346e8cafb30ac25ab6190d
Richie-Yang <richieyang8813@gmail.com>
2022-01-12 21:21:56 +0800

    feat: finish 2, 3, 3.1 files

--- 2. async_callback.js
+++ 2. async_callback.js
@@ -11,10 +11,23 @@ db.once('open', () => {
   for (const [user_index, user] of users.entries()) {
     // 創建使用者資料(user): model.create
     UserModel.create(user, (err, user) => {
+      console.log(`使用者${user_index}已經建立完成`)
+
+      restaurantArray = []
+      restaurants.forEach((item, index) => {
+        if (index >= user_index * 3 && index < (user_index + 1) * 3) {
+          item.userId = user._id
+          console.log(`前置餐廳${index}已經成功置入使用者${user_index}`)
+          restaurantArray.push(item)
+        }
+      })
+      console.log(`前置完成餐廳已經置入使用者資料`)
+
       // 對每個user建立相對應餐廳資料
       RestaurantModel.create(restaurants, (err, user) => {
         // 等待所有使用者的餐廳資料創建完成
         console.log('所有使用者與餐廳資料創建完成')
+        process.exit()
       })
     })
   }
